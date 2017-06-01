3bbadc225d74963189266d26d0413ffd98f2f263
AnnAnnFryingPan <AnnAnnFryingPan@users.noreply.github.com>
2017-06-01 16:39:33 +0100

    Add files via upload

--- data_sources/osisoft_pi_sources/ReadMe.txt
+++ data_sources/osisoft_pi_sources/ReadMe.txt
@@ -1,8 +1,21 @@
 ***** list_osisoft-pi_streams.csv format: *****
 
-[url],[user's feed name]
+[core api url], [data type*], [params], [request type**], [user's feed name]
+
+*Data type options:
+    streams = 1
+    streamsets = 2
+    elements = 3
+    attributes = 4
+
+**Request type options:     
+    none = 0
+    plot = 1
+    interpolated = 2
+    summary = 3
+    recorded = 4
 
 
 ***** Eg. *****
 
-https://130.88.97.137/piwebapi/streamsets/E0XpbRmwnc7kq0OSy1LydJJQxey0y1BT5hGA3gBQVqtCQgVk0tUEktUDAxLkRTLk1BTi5BQy5VS1xUUklBTkdVTFVNXEFJUiBRVUFMSVRZXE9YRk9SRCBST0FE/plot,Anns_Pi_feed_Nitrogen
+https://130.88.97.137/piwebapi,streamsets,E0XpbRmwnc7kq0OSy1LydJJQxey0y1BT5hGA3gBQVqtCQgVk0tUEktUDAxLkRTLk1BTi5BQy5VS1xUUklBTkdVTFVNXEFJUiBRVUFMSVRZXE9YRk9SRCBST0FE,plot,Anns_Pi_feed_Nitrogen
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from tendo import singleton
 import sys
 import datetime
 import os
+import json
 
 sys.path.insert(0, '../data_hub_call')
 sys.path.insert(0, '../influxdb_connection')
@@ -21,6 +22,7 @@ osisoft_pi_sources_dir = 'osisoft_pi_sources'
 osisoft_pi_streams_file = 'list_osisoft-pi_streams.csv'
 
 
+
 def main():
     try:
         if (len(sys.argv) > 0):
@@ -31,28 +33,31 @@ def main():
         print('Error reading command line argument')
         raise
 
+    api_streams = Data_stream_fetch_list()
+
+    with open(os.path.join(streams_dir, hypercat_sources_dir, hypercat_credentials_file)) as f_creds:
+        credentials_file = f_creds.readlines()
+        list_params = credentials_file[0].split(",")
+        hypercat_api_key = list_params[0]
+        hypercat_username = list_params[1].rstrip('\n')
+
+    with open(os.path.join(streams_dir, hypercat_sources_dir, hypercat_streams_file)) as f_streams:
+        api_streams_file = f_streams.readlines()
+    api_streams.append_hypercat_stream_list(hypercat_username, hypercat_api_key, api_streams_file)
+
+    with open(os.path.join(streams_dir, osisoft_pi_sources_dir, osisoft_pi_streams_file)) as f_streams:
+        api_streams_file = f_streams.readlines()
+    api_streams.append_pi_stream_list(api_streams_file)
+
+
     running = True
     while running:
         start = time.clock()
 
-        api_streams = Data_stream_fetch_list()
+        print("")
+        print("***** No. of streams to be processed: " + str(len(api_streams)) + "*****")
 
-        with open(os.path.join(streams_dir, hypercat_sources_dir, hypercat_credentials_file)) as f_creds:
-            credentials_file = f_creds.readlines()
-            list_params = credentials_file[0].split(",")
-            hypercat_api_key = list_params[0]
-            hypercat_username = list_params[1].rstrip('\n')
-        with open(os.path.join(streams_dir, hypercat_sources_dir, hypercat_streams_file)) as f_streams:
-            api_streams_file = f_streams.readlines()
-        api_streams.append_hypercat_stream_list(hypercat_username, hypercat_api_key, api_streams_file)
-
-        with open(os.path.join(streams_dir, osisoft_pi_sources_dir, osisoft_pi_streams_file)) as f_streams:
-            api_streams_file = f_streams.readlines()
-        api_streams.append_pi_stream_list(api_streams_file)
-
-
-        print("No. of streams to be processed: " + str(len(api_streams)))
-        print(datetime.datetime.now())
+        influx_db_connection = influxdb_connection.Influxdb_connection(influxdb_db_name)
 
         for stream in api_streams.streams:
 
@@ -60,37 +65,54 @@ def main():
             if(stream.api_core_url == Data_hub_call_bt.core_URL):
                 try:
                     bt_hub = Data_hub_call_bt(stream)
-                    bt_hub_response = bt_hub.call_api_fetch(stream.params)
-                    print("BT-Hub call successful. " + str(len(bt_hub_response)) + " returned rows from " + stream.users_feed_name)
-                except:
-                    print("Error calling Hub: " + stream.api_core_url)
-                try:
-                    influx_db_connection = influxdb_connection.Influxdb_connection(influxdb_db_name)
-                    influx_db_connection.import_hypercat_response_json(bt_hub_response.decode("utf-8"),
-                                                                       stream.users_feed_name)
-                    print("influxDb call successful: To " + stream.users_feed_name)
-                    # print(influx_db_connection.query_database('select value from ' + stream.users_feed_name + ';'))
-                except:
-                    print("Error populating influx-db")
+                    cur_params = stream.params
+
+                    bt_hub_response = bt_hub.call_api_fetch(cur_params)
+
+                    if (bt_hub_response.ok):
+                        print("BT-Hub call successful. " + str(
+                            len(bt_hub_response.content)) + " returned rows from " + stream.users_feed_name)
+                        print(bt_hub_response.content)
+
+                        try:
+                            influx_db_connection.import_hypercat_response_json(bt_hub_response.content.decode("utf-8"),
+                                                                               stream.users_feed_name)
+                            print("influxDb call successful: To " + stream.users_feed_name)
+                            print(influx_db_connection.query_database(
+                                'select value from ' + stream.users_feed_name + ';'))
+                        except Exception as err:
+                            print("Error populating influx-db: " + str(err))
+                    else:
+                        print("Error: call to hub: " + stream.api_core_url + " failed with status code: " + \
+                               bt_hub_response.reason)
+
+                except Exception as err:
+                    print("Error calling Hub: " + stream.api_core_url + ". " + str(err))
+
+
 
             elif(stream.api_core_url == Data_hub_call_osisoft_pi.core_URL):
                 try:
                     pi_hub = Data_hub_call_osisoft_pi(stream)
                     pi_hub_response = pi_hub.call_api_fetch(stream.params)
-                    print("Pi-Hub call successful. " + str(
-                        len(pi_hub_response)) + " returned rows from " + stream.users_feed_name)
+                    if (pi_hub_response.ok):
+                        print("Pi-Hub call successful. " + str(
+                            len(pi_hub_response.content)) + " returned rows from " + stream.users_feed_name)
+
+                        try:
+                            influx_db_connection.import_pi_response_json(pi_hub_response.content.decode("utf-8"),
+                                                                         stream.users_feed_name)
+                            print("influxDb call successful: To " + stream.users_feed_name)
+                            # print(influx_db_connection.query_database('select value from ' + stream.users_feed_name + ';'))
+                        except Exception as err:
+                            print("Error populating influx-db: " + str(err))
+                    else:
+                        print("Error: call to hub: " + stream.api_core_url + " failed with status code:  " + \
+                              pi_hub_response.reason)
+
                     #print(pi_hub_response)
                 except:
                     print("Error calling Pi hub: " + stream.api_core_url)
-
-                try:
-                    influx_db_connection = influxdb_connection.Influxdb_connection(influxdb_db_name)
-                    influx_db_connection.import_pi_response_json(pi_hub_response.decode("utf-8"),
-                                                                       stream.users_feed_name)
-                    print("influxDb call successful: To " + stream.users_feed_name)
-                    # print(influx_db_connection.query_database('select value from ' + stream.users_feed_name + ';'))
-                except:
-                    print("Error populating influx-db")
 
 
 
