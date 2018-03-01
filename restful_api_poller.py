05ebdf46694bb2234cd8e2e002d1bfe54fe3c857
Ann Gledson <ann.gledson@manchester.ac.uk>
2018-03-01 19:17:52 +0000

    Handles CDP requests and input test files updated

--- restful_api_poller.py
+++ restful_api_poller.py
@@ -46,13 +46,11 @@ class Restful_api_poller(Poller):
         self.api_requests = Request_info_fetch_list()
         self.influx_db = None
 
+        self.read_files()
+
 
 
     def do_work(self):
-        self.api_requests.clear_all()
-
-        self.read_files()
-
         if (len(self.api_requests) == 0):
             print('Unable to read any streams data from input home directory. Exiting.')
             sys.exit(0)
@@ -69,6 +67,7 @@ class Restful_api_poller(Poller):
 
 
     def read_files(self):
+        self.api_requests.clear_all()
         try:
             with open(os.path.join(self.requests_dir, RESTFUL_BT_SOURCES_DIR, BT_HUB_CREDENTIALS_FILE)) as f_creds:
                 credentials_file = f_creds.readlines()
@@ -88,6 +87,26 @@ class Restful_api_poller(Poller):
             except Exception as err:
                 print('Unable to read BT streams file: ' + os.path.join(self.requests_dir, RESTFUL_BT_SOURCES_DIR,
                                                                         RESTFUL_BT_REQUESTS_FILE) + str(err))
+
+        try:
+            with open(os.path.join(self.requests_dir, CDP_SOURCES_DIR, CDP_CREDENTIALS_FILE)) as f_creds:
+                credentials_file = f_creds.readlines()
+                list_params = credentials_file[0].split(",")
+                cdp_api_key = list_params[0].rstrip('\n')
+                #cdp_username = list_params[1].rstrip('\n')
+        except Exception as err:
+            print('Unable to read CDP credentials file: ' + os.path.join(self.requests_dir, CDP_SOURCES_DIR,
+                                                                        CDP_CREDENTIALS_FILE))
+        else:
+            try:
+                with open(os.path.join(self.requests_dir, CDP_SOURCES_DIR,
+                                       CDP_REQUESTS_FILE)) as f_requests:
+                    api_requests_file = f_requests.readlines()
+                    self.api_requests.append_restful_cdp_request_list(cdp_api_key,
+                                                                     api_requests_file)
+            except Exception as err:
+                print('Unable to read CDP streams file: ' + os.path.join(self.requests_dir, CDP_SOURCES_DIR,
+                                                                        CDP_REQUESTS_FILE) + str(err))
 
 
         try:
@@ -115,7 +134,16 @@ class Restful_api_poller(Poller):
                 short_name = 'Triangulum'
             elif request.api_core_url == Data_hub_call_restful_cdp.core_URL:
                 hub = Data_hub_call_restful_cdp(request)
-                hub_response = hub.call_api_fetch(request.params, get_latest_only=self.get_latest_only)
+                if 'time_field' in request.feed_info:
+                    get_children_as_time_series = True
+                    time_field = request.feed_info['time_field']
+                else:
+                    get_children_as_time_series = False
+                    time_field = ''
+                hub_response = hub.call_api_fetch(request.params,
+                                                  get_latest_only=self.get_latest_only,
+                                                  get_children_as_time_series=get_children_as_time_series,
+                                                  time_field=time_field)
                 short_name = 'CDP'
             csv_output_prefix = short_name + '_'
             print(short_name + ' hub response: ' + str(hub_response))
@@ -130,9 +158,8 @@ class Restful_api_poller(Poller):
 
             if (self.influx_db != None):
                 try:
-                    if request.api_core_url == Data_hub_call_restful_bt.core_URL or \
-                        request.api_core_url == Data_hub_call_restful_cdp.core_URL:
-                        self.influx_db.import_restful_response_json(
+                    if request.api_core_url == Data_hub_call_restful_bt.core_URL:
+                        self.influx_db.import_restful_bt_response_json(
                             hub_response['content'],
                             request.users_feed_name,
                             request.feed_info)
@@ -141,17 +168,22 @@ class Restful_api_poller(Poller):
                             hub_response['content'],
                             request.users_feed_name,
                             request.feed_info)
-                    print(short_name + " call successful: To table: " +
+                    elif request.api_core_url == Data_hub_call_restful_cdp.core_URL:
+                        self.influx_db.import_restful_cdp_response_json(
+                            hub_response['content'],
+                            request.users_feed_name,
+                            request.feed_info)
+                    print("Influx-db call successful: Import to influx table: " +
                           request.users_feed_name + " in " + self.influxdb_db_name)
                 except Exception as err:
                     print("Error populating " + short_name + ": " + str(err))
-                else:
+                """else:
                     try:
                         print('select * from ' + request.users_feed_name + ": " +
                               str(self.influx_db.query_database(
                                   'select * from ' + request.users_feed_name + ';')))
                     except Exception as err:
-                        print("Error reading new data from influx-db.")
+                        print("Error reading new data from influx-db.")"""
 
             else:
                 file_spec = os.path.join(self.output_dir,
