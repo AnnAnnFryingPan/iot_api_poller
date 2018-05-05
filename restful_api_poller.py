d880fc6496df5347d69bb3d5bdd7259d41e3913e
annannfryingpan <anngledson@gmail.com>
2018-05-05 09:19:13 +0100

    Added data_hub_call factory

--- restful_api_poller.py
+++ restful_api_poller.py
@@ -6,22 +6,7 @@ sys.path.insert(0, '../influxdb_connection')
 
 from selected_streams import Selected_streams
 from poller import Poller
-
-from data_hub_call_restful_bt import Data_hub_call_restful_bt
-from data_hub_call_restful_cdp import Data_hub_call_restful_cdp
-from data_hub_call_osisoft_pi import Data_hub_call_osisoft_pi
-
-
-RESTFUL_BT_SOURCES_DIR = "restful_bt_sources"
-BT_HUB_CREDENTIALS_FILE = 'bt_hub_credentials.csv'
-RESTFUL_BT_REQUESTS_FILE = 'list_restful_bt_requests.csv'
-CDP_SOURCES_DIR = "cdp_sources"
-CDP_CREDENTIALS_FILE = 'cdp_credentials.csv'
-CDP_REQUESTS_FILE = 'list_cdp_requests.csv'
-TRIANGULUM_SOURCES_DIR = 'osisoft_pi_sources'
-TRIANGULUM_REQUESTS_FILE = 'list_osisoft-pi_requests.csv'
-
-
+from data_hub_call_factory import Data_hub_call_factory
 
 try:
     import influxdb_connection
@@ -35,7 +20,6 @@ class Restful_api_poller(Poller):
     INPUT_DIR = 'data_sources'
     CSV_OUTPUT_DIR = 'output'
     DEFAULT_POLLER_ID = 'default-id'
-
 
     def __init__(self, home_dir, influxdb_db_name, polling_interval, get_latest_only=True):
         if not os.path.isdir(home_dir):
@@ -56,16 +40,14 @@ class Restful_api_poller(Poller):
         except Exception as err:
             raise err
 
-
-        if (len(self.selected_streams.api_streams.requests) == 0):
+        if len(self.selected_streams.api_streams.requests) == 0:
             raise IOError('Unable to read any streams data from input home directory. Exiting.')
-
 
     def do_work(self):
         print("")
         print("***** No. of streams to be processed: " + str(len(self.selected_streams.api_streams.requests)) + " *****")
 
-        if (str(self.influxdb_db_name).strip().lower() != 'file'):
+        if str(self.influxdb_db_name).strip().lower() != 'file':
             self.influx_db = influxdb_connection.Influxdb_connection(self.influxdb_db_name)
 
         for request in self.selected_streams.api_streams.requests:
@@ -75,17 +57,49 @@ class Restful_api_poller(Poller):
             except Exception as err:
                 print("Not able to poll " + request.users_feed_name + ' at this time. Continuing poller. ' + str(err))
 
-
     def poll_hub(self, request):
+        try:
+            hub = Data_hub_call_factory.create_data_hub_call(request)
+            hub_response = hub.call_api_fetch(request.params, get_latest_only=self.get_latest_only)
+            print(hub.hub_id + ' hub response: ' + str(hub_response))
+        except Exception as err:
+            raise err
+
+        print(hub.hub_id + " call successful. " + str(str(hub_response['returned_matches'])) +
+              " returned rows from " + request.users_feed_name)
+
+        if self.influx_db is not None:
+            try:
+                self.influx_db.import_restful_api_response(
+                    hub.get_influx_db_import_json(
+                        hub_response['content'],
+                        request.users_feed_name,
+                        request.feed_info))
+                print("Influx-db call successful: Import to influx table: " +
+                      request.users_feed_name + " in " + self.influxdb_db_name)
+            except Exception as err:
+                print("Error populating Influx with " + request.hub_id + " data: " + str(err))
+
+        else:
+            file_spec = os.path.join(self.output_dir,
+                                     request.hub_id + '_' + request.users_feed_name + '.json')
+            try:
+                with open(file_spec, 'a+') as csv_file:
+                    csv_file.write(hub_response['content'] + '\n')
+                print("csv file write successful: To file: " + file_spec)
+            except Exception as err:
+                print('Unable to write to output file ' + file_spec + '. ' + str(err))
+
+        """        
         if request.api_core_url == Data_hub_call_restful_bt.core_URL:
             self.poll_bt_hub(request)
         elif request.api_core_url == Data_hub_call_restful_cdp.core_URL:
             self.poll_cdp_hub(request)
         elif request.api_core_url == Data_hub_call_osisoft_pi.core_URL:
-            self.poll_pi_hub(request)
+            self.poll_pi_hub(request)"""
 
 
-    def poll_bt_hub(self, request):
+    """def poll_bt_hub(self, request):
         try:
             hub = Data_hub_call_restful_bt(request)
             hub_response = hub.call_api_fetch(request.params, get_latest_only=self.get_latest_only)
@@ -115,7 +129,7 @@ class Restful_api_poller(Poller):
                     csv_file.write(hub_response['content'] + '\n')
                 print("csv file write successful: To file: " + file_spec)
             except Exception as err:
-                print('Unable to write to output file .' + str(err))
+                print('Unable to write to output file .' + str(err)) 
 
 
     def poll_cdp_hub(self, request):
@@ -192,4 +206,4 @@ class Restful_api_poller(Poller):
                     csv_file.write(hub_response['content'] + '\n')
                 print("csv file write successful: To file: " + file_spec)
             except Exception as err:
-                print('Unable to write to output file .' + str(err))
+                print('Unable to write to output file .' + str(err))"""
