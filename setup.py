34746b2e7b75ff755886593d3bf2845a17440b9b
annannfryingpan <anngledson@gmail.com>
2018-07-11 17:05:35 +0100

    First go at packaging modules, incl. slight directory restructure

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='iot_api_poller',
+      version='0.1',
+      description='Polling app that fetches data from IoT hubs',
+      url='https://github.com/AnnAnnFryingPan/iot_api_poller',
+      author='Ann Gledson',
+      author_email='anngledson@gmail.com',
+      license='MIT',
+      packages=['iot_api_poller'],
+      zip_safe=False)
+
